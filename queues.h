a387e6441dfa3bff7678c33992f91a7b3b349eae
Kirsten S <daniellesison19@gmail.com>
2019-07-04 22:07:15 +0800

    Finished Stack Operations, Added create Queue

--- queues.h
+++ queues.h
@@ -5,6 +5,10 @@
 #ifndef STACKS_QUEUES_H
 #define STACKS_QUEUES_H
 #endif //STACKS_QUEUES_H
+typedef struct que {
+    Stack queue;
+    int queue_size;
+} Queue;
 
 /* Queue Operations
  * enqueue() - removes an element from the queue and returns it
@@ -13,8 +17,19 @@
  * isEmpty() - checks if stack is empty
  * isFull() - checks if stack is full
  * */
+Queue createQueue(int size) {
+    Queue Q; Stack stack;
+    if (size > 2) {
+        stack.stack_size = size;
+        Q.queue_size = size;
+        Q.queue = stack;
+    } else
+        printf("Size is too small for a queue!\n");
+    return Q;
+}
 
-void enqueue(int data);
+void enqueue(int data) {
+}
 void dequeue();
 int front();
 int isEmpty(); /* return 1 if true 0 if false */
--- stacks.h
+++ stacks.h
@@ -6,22 +6,16 @@
 #define STACKS_STACKS_N_QUEUES_H
 
 #endif //STACKS_STACKS_N_QUEUES_H
+#define MAX 100
+
 
 typedef  struct stk {
-    int size;
-    int *stack_data;
+    int stack_size;
+    int stack_data[MAX];
     int top1;
     int top2;
 
 } Stack;
-
-typedef struct que {
-    int QMAX;
-    int *queue_data;
-    int front;
-    int back;
-    int numInQueue;
-} Queue;
 
 /* Stack Operations
  * create() - creates 2 stacks
@@ -31,31 +25,45 @@ typedef struct que {
  * isEmpty() - checks if stack is empty
  * isFull() - checks if stack is full
  * */
-Stack create(int size) {
-    Stack stack; int arr[size];
+Stack createStack(int size) {
+    Stack stack;
     if (size > 1) {
-        stack.size = size;
-        stack.stack_data = arr;
+        stack.stack_size = size;
         stack.top1 = -1;
         stack.top2 = size;
     } else
         printf("Size is too small for two stacks.\n");
     return stack;
 }
+
+int isFull1(Stack stack) {
+    if (stack.top1+1 == stack.top2 || stack.top1 == stack.stack_size-1)
+        return 1;
+    else
+        return 0;
+}
+
+int isFull2(Stack stack) {
+    printf("%d , %d\n", stack.top2-1, stack.top1);
+    if (stack.top2-1 == stack.top1 || stack.top2 == 0)
+        return 1;
+    else
+        return 0;
+}
+
 void push1(int data, Stack *stack) {
-    if (stack->top1 < stack->top2 && stack->top1 < stack->size) {
+
+    if (!(isFull1(*stack))) {
         (stack->top1)++;
         stack->stack_data[stack->top1] = data;
-        printf("%d at top %d\n",  stack->stack_data[stack->top1], stack->top1);
     }
     else
         printf("Stack 1 is full.\n");
 }
 void push2(int data, Stack *stack) {
-    if (stack->top2 > stack->top1 && stack->top2 >= 0) {
+    if (!(isFull2(*stack))) {
         (stack->top2)--;
         stack->stack_data[stack->top2] = data;
-        printf("%d at top %d\n",  stack->stack_data[stack->top2], stack->top2);
     }
     else
         printf("Stack 2 is full.\n");
@@ -65,7 +73,7 @@ int pop1(Stack *stack) {
     int num;
     if (stack->top1 == -1) {
         printf("Stack underflow\n");
-        return NULL;
+        return -1;
     } else {
         num = stack->stack_data[stack->top1];
         stack->top1--;
@@ -75,9 +83,9 @@ int pop1(Stack *stack) {
 
 int pop2(Stack *stack) {
     int num;
-    if (stack->top2 > stack->size) {
+    if (stack->top2 > stack->stack_size) {
         printf("Stack underflow\n");
-        return NULL;
+        return -1;
     } else {
         num = stack->stack_data[stack->top2];
         stack->top2++;
@@ -85,40 +93,32 @@ int pop2(Stack *stack) {
     }
 }
 
-int isFull1(Stack *stack) {
-    if (stack->top1 == -1)
+int isEmpty1(Stack stack) {
+    if (stack.top1 == -1)
         return 1;
     else
         return 0;
 }
 
-int isFull2(Stack *stack) {
-    if (stack->top2 == stack->size-1)
+int isEmpty2(Stack stack) {
+    if (stack.top2 == stack.stack_size)
         return 1;
     else
         return 0;
 }
 
-int isEmpty1(Stack *stack) {
-    if (stack->top1 == stack->top2)
-        return 1;
-    else if (stack->top1 > stack->top2)
-        return 1;
-    else if(stack->top1 >= stack->size)
-        return 1;
+int top1(Stack stk) {
+    if (stk.top1 != -1 || stk.top1 != stk.stack_size)
+        return stk.stack_data[stk.top1];
     else
-        return 0;
+        return -1;
 }
 
-int isEmpty2(Stack *stack) {
-    if (stack->top2 == stack->top1)
-        return 1;
-    else if(stack->top2 < stack->top1)
-        return 1;
-    else if (stack->top2 < 0)
-        return 1;
+int top2(Stack stk) {
+    if (stk.top2 != stk.stack_size || stk.top1 != -1)
+        return stk.stack_data[stk.top2];
     else
-        return 0;
+        return -1;
 }
 
 void display1(Stack stk) {
@@ -130,7 +130,7 @@ void display1(Stack stk) {
 
 void display2(Stack *stk) {
     int i;
-    for (i=stk->top2; i < stk->size; i++)
+    for (i=stk->top2; i < stk->stack_size; i++)
         printf("%d @ idx %d\n", stk->stack_data[i], i);
     printf("\n");
 }
